0d0016c08d40c0e3d69d2359d03366b9e1401b2f
Rishabh <cs19mtech11026@iith.ac.in>
2021-06-19 22:51:36 +0530

    Making modification in ikev2_auth.c for EDDSA. (Identity hash algorithm is used.)

--- include/ike_alg_hash.h
+++ include/ike_alg_hash.h
@@ -18,7 +18,7 @@
 #ifdef USE_SHA1
 extern const struct hash_desc ike_alg_hash_sha1;
 #endif
-
+extern const struct hash_desc ike_alg_hash_identity;
 extern const struct hash_desc ike_alg_hash_sha2_256;	/* also used for cookies */
 #ifdef USE_SHA2
 extern const struct hash_desc ike_alg_hash_sha2_384;
