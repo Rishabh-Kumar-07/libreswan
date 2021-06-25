c172dbb41e94db11483cea9c335ecc4c30400fde
Rishabh <cs19mtech11026@iith.ac.in>
2021-06-25 21:40:34 +0530

    Making crypt_mac dynamic and loading the bytes.

--- include/crypt_hash.h
+++ include/crypt_hash.h
@@ -73,4 +73,6 @@ PK11SymKey *crypt_hash_symkey(const char *hash_name,
 			      const char *symkey_name, PK11SymKey *symkey,
 			      struct logger *logger);
 
+void crypt_mac_load(struct crypt_mac *container, chunk_t packet);
+
 #endif
