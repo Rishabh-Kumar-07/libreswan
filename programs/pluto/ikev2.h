486c8081b4493019f740995ea29f0bf7bb1d6ae3
Rishabh <cs19mtech11026@iith.ac.in>
2021-07-25 19:27:21 +0530

    EdDSA support.

--- programs/pluto/ikev2.h
+++ programs/pluto/ikev2.h
@@ -140,6 +140,12 @@ diag_t v2_authsig_and_log_using_ECDSA_pubkey(struct ike_sa *ike,
 					     shunk_t signature,
 					     const struct hash_desc *hash_algo);
 
+diag_t v2_authsig_and_log_using_EDDSA_pubkey(struct ike_sa *ike,
+					     const struct crypt_mac *idhash,
+					     shunk_t signature,
+					     const struct hash_desc *hash_algo);
+
+
 extern void ikev2_derive_child_keys(struct ike_sa *ike, struct child_sa *child);
 
 void v2_schedule_replace_event(struct state *st);
