828b2bdb732bd09e732e08a4bd9262adf4196663
Rishabh <cs19mtech11026@iith.ac.in>
2021-08-11 12:31:18 +0530

    Adding EDDSA files and test suite.

--- lib/libswan/crypt_hash.c
+++ lib/libswan/crypt_hash.c
@@ -107,7 +107,7 @@ void crypt_hash_final_bytes(struct crypt_hash **hashp,
 
 void crypt_mac_load(struct crypt_mac *container, chunk_t packet){
     for(size_t i = 0; i < packet.len; i++){
-        (container->y)[i + container->len] = (packet.ptr)[i];
+        (container->ptr)[i + container->len] = (packet.ptr)[i];
     }
     container->len += packet.len;
 }
